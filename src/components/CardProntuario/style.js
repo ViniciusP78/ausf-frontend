da201102585e7373f8da5fbe3e9aa64cd45a7da4
mauricio medeiros <mauricio_medeiros10@hotmail.com>
2021-04-27 19:12:30 -0300

    ajuste no button do card de prontuarios

--- src/components/CardProntuario/style.js
+++ src/components/CardProntuario/style.js
@@ -10,6 +10,14 @@ export default makeStyles((theme) => ({
   },
   colorSecondary:{
     color:theme.palette.secondary.main
+  },
+  buttonMui:{
+    padding:0,
+    width:'100%',
+    height:'100%',
+    boxShadow:'none',
+    backgroundColor:theme.palette.grey.light,
+    borderRadius:0,
   }
 }));
 
@@ -25,13 +33,14 @@ export const Container = styled.div`
     margin-bottom:18px
   }
 `;
-export const Button = styled.div`
+export const Button = styled.button`
   display:flex;
   font-weight:500;
   height:50%;
   width:100%;
   justify-content:space-around;
   align-items:center;
+  border: 0;
   border-left: 2px solid ${({theme}) => (theme.border.main)};
   background-color:${({theme}) => (theme.grey.light)};
   &:first-child{
